bf6581a9e0f3de45b71e882b3859422d8601bf22
JP-Conde5 <jcgalves25@hotmail.com>
2023-05-19 10:40:32 -0300

    Primeira aula de JS

---
+++ codigo.js
@@ -0,0 +1,13 @@
+const listElement = document.querySelector("#app ul")
+const inputElement = document.querySelector("#app input")
+const buttonElement = document.querySelector("#app button")
+
+const tarefas = ["Estudar", "Dormir"]
+
+for(cosnt itemTarefa of tarefas){
+    const tarefaElement = document.createElement("li")
+    const tarefaText = document.createTextNode(itemTarefa)
+
+    tarefaElement.appendChild(tarefaText)
+    listElement.appendChild(tarefaElement)
+}
