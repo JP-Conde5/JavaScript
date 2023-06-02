fa80703e9b77d8aa38885bd2ae54251258ff52fc
JP-Conde5 <jcgalves25@hotmail.com>
2023-06-02 10:20:44 -0300

    Pronto

--- codigo.js
+++ codigo.js
@@ -2,12 +2,43 @@ const listElement = document.querySelector("#app ul")
 const inputElement = document.querySelector("#app input")
 const buttonElement = document.querySelector("#app button")
 
-const tarefas = ["Estudar", "Dormir"]
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+function mostrarTarefas(){
+    listElement.innerHTML = ""
+    for(const itemTarefa of tarefas){
+        const tarefaElement = document.createElement("li")
+        const tarefaText = document.createTextNode(itemTarefa)
+        const linkElement = document.createElement("a")
+        linkElement.setAttribute("href", "#")
+        const pos = tarefas.indexOf(itemTarefa)
+        linkElement.setAttribute("onclick", `deleteTarefa(${pos})`)
+        const linkText = document.createTextNode("Excluir")
+        linkElement.appendChild(linkText)
 
-for(cosnt itemTarefa of tarefas){
-    const tarefaElement = document.createElement("li")
-    const tarefaText = document.createTextNode(itemTarefa)
+        tarefaElement.appendChild(tarefaText)
+        tarefaElement.appendChild(linkElement)
+        listElement.appendChild(tarefaElement)
+    }    
+}
 
-    tarefaElement.appendChild(tarefaText)
-    listElement.appendChild(tarefaElement)
+mostrarTarefas()
+
+function addTarefas(){
+    const text = inputElement.value
+    tarefas.push(text)
+    mostrarTarefas()
+    inputElement.value = ""
+    saveToStorage()
+}
+
+buttonElement.onclick = addTarefas
+
+function deleteTarefa(pos){
+    tarefas.splice(pos, 1)
+    mostrarTarefas()
+    saveToStorage()
+}
+
+function saveToStorage(){
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
 }
